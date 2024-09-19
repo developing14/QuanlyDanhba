c7b59424222dd73900f6b74e2ce7b7fd98c7c5f0
quang <quangb2016996@student.ctu.edu.vn>
2024-09-19 19:40:09 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -14,4 +14,15 @@ const contactsRouter = require("./app/routes/contact.route");
 
 app.use("/api/contacts", contactsRouter);
 
+const ApiError = require("./app/api-error");
+
+//handle 404 response
+app.use((req, res, next)=> {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json ({message: error.message || "Internal Server Error"})
+})
+
 module.exports = app;
